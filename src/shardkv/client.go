91f68b4a9ffb4ee78324504d4ba2c986f08aea7f
woozway <22060468@zju.edu.cn>
2022-03-28 22:54:35 +0800

    shardkv

--- src/shardkv/client.go
+++ src/shardkv/client.go
@@ -8,11 +8,14 @@ package shardkv
 // talks to the group that holds the key's shard.
 //
 
-import "../labrpc"
-import "crypto/rand"
-import "math/big"
-import "../shardmaster"
-import "time"
+import (
+	"crypto/rand"
+	"math/big"
+	"time"
+
+	"../labrpc"
+	"../shardmaster"
+)
 
 //
 // which shard is a key in?
@@ -40,6 +43,11 @@ type Clerk struct {
 	config   shardmaster.Config
 	make_end func(string) *labrpc.ClientEnd
 	// You will have to modify this struct.
+	clientId int64
+}
+
+func (ck *Clerk) getConfigNum() int {
+	return ck.config.Num
 }
 
 //
@@ -56,6 +64,7 @@ func MakeClerk(masters []*labrpc.ClientEnd, make_end func(string) *labrpc.Client
 	ck.sm = shardmaster.MakeClerk(masters)
 	ck.make_end = make_end
 	// You'll have to add code here.
+	ck.clientId = nrand()
 	return ck
 }
 
@@ -67,9 +76,12 @@ func MakeClerk(masters []*labrpc.ClientEnd, make_end func(string) *labrpc.Client
 //
 func (ck *Clerk) Get(key string) string {
 	args := GetArgs{}
+	args.MsgId = nrand()
+	args.ClientId = ck.clientId
 	args.Key = key
 
 	for {
+		args.ConfigNum = ck.getConfigNum()
 		shard := key2shard(key)
 		gid := ck.config.Shards[shard]
 		if servers, ok := ck.config.Groups[gid]; ok {
@@ -101,12 +113,14 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	args := PutAppendArgs{}
+	args.MsgId = nrand()
+	args.ClientId = ck.clientId
 	args.Key = key
 	args.Value = value
 	args.Op = op
 
-
 	for {
+		args.ConfigNum = ck.getConfigNum()
 		shard := key2shard(key)
 		gid := ck.config.Shards[shard]
 		if servers, ok := ck.config.Groups[gid]; ok {
@@ -132,6 +146,7 @@ func (ck *Clerk) PutAppend(key string, value string, op string) {
 func (ck *Clerk) Put(key string, value string) {
 	ck.PutAppend(key, value, "Put")
 }
+
 func (ck *Clerk) Append(key string, value string) {
 	ck.PutAppend(key, value, "Append")
 }
---
+++ src/shardkv/server_op.go
@@ -0,0 +1,90 @@
+package shardkv
+
+import (
+	"time"
+)
+
+type Op struct {
+	// Your definitions here.
+	// Field names must start with capital letters,
+	// otherwise RPC will break.
+	Key       string
+	Value     string
+	Op        string // "Put" or "Append" Get,
+	ClientId  int64
+	MsgId     int64
+	ReqId     int64
+	ConfigNum int
+}
+
+type NotifyMsg struct {
+	Err   Err
+	Value string
+}
+
+func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
+	op := Op{
+		MsgId:     args.MsgId,
+		ReqId:     nrand(),
+		Key:       args.Key,
+		Op:        "Get",
+		ClientId:  args.ClientId,
+		ConfigNum: args.ConfigNum,
+	}
+	res := kv.waitCmd(op)
+	reply.Err = res.Err
+	reply.Value = res.Value
+}
+
+func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
+	op := Op{
+		MsgId:     args.MsgId,
+		ReqId:     nrand(),
+		Key:       args.Key,
+		Value:     args.Value,
+		Op:        args.Op,
+		ClientId:  args.ClientId,
+		ConfigNum: args.ConfigNum,
+	}
+	reply.Err = kv.waitCmd(op).Err
+}
+
+func (kv *ShardKV) removeCh(id int64) {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	delete(kv.notifyCh, id)
+}
+
+func (kv *ShardKV) waitCmd(op Op) (res NotifyMsg) {
+	ch := make(chan NotifyMsg, 1)
+
+	kv.mu.Lock()
+	if op.ConfigNum == 0 || op.ConfigNum < kv.config.Num {
+		res.Err = ErrWrongGroup
+		kv.mu.Unlock()
+		return
+	}
+	kv.mu.Unlock()
+
+	_, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		res.Err = ErrWrongLeader
+		return
+	}
+
+	kv.mu.Lock()
+	kv.notifyCh[op.ReqId] = ch
+	kv.mu.Unlock()
+
+	t := time.NewTimer(WaitCmdTimeOut)
+	defer t.Stop()
+	select {
+	case res = <-ch:
+		kv.removeCh(op.ReqId)
+		return
+	case <-t.C:
+		kv.removeCh(op.ReqId)
+		res.Err = ErrTimeOut
+		return
+	}
+}
